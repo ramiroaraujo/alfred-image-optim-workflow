ff70c1bfb9c8e2eda74316e508364975b4a224c4
Ramiro Araujo <rama.araujo@gmail.com>
2014-01-06 15:26:53 -0300

    corrected bug in file type detection removed console formatting for notifications output

--- workflow/main.rb
+++ workflow/main.rb
@@ -10,8 +10,8 @@ imagealpha = false
 # checks if JPEGs or PNGs are present, and enable jpeg-mini and ImageAlpha accordingly
 files.each do |filepath|
   ext = File.extname(filepath)
-  jpegmini = true if ext == 'jpg' || ext == 'jpeg'
-  imagealpha = true if ext == 'png'
+  jpegmini = true if ext == '.jpg' || ext == '.jpeg'
+  imagealpha = true if ext == '.png'
 end
 
 # joins back the files
@@ -20,5 +20,5 @@ selection = files.join "\n"
 # runs ImageOptim-CLI
 output = `echo "#{selection}" | bin/imageOptim #{'--jpeg-mini' if jpegmini} #{'--image-alpha' if imagealpha} --quit`
 
-# outputs last line of feedback, which shows savings
-puts output.split("\n")[-1]
+# outputs last line of feedback (and removes console formatting), which shows savings
+puts output.split("\n")[-1].gsub(27.chr, '').gsub(/\[31m|\[32m|\[39m/, '')
